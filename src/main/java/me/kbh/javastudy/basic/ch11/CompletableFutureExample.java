a4b5df43dcde5c5d67287720022cf1aa682f5317
jvm-kbh <elasticsearch@kakao.com>
2023-10-25 18:58:04 +0900

    java - CompletableFuture 학습 - 다중 파이프라인으로 lazy evaluation을 피하고 비동기적으로 작업 수행해보

---
+++ src/main/java/me/kbh/javastudy/basic/ch11/CompletableFutureExample.java
@@ -0,0 +1,80 @@
+package me.kbh.javastudy.basic.ch11;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class CompletableFutureExample {
+    /*public static void main(String[] args) {
+        // 가정: entityList는 1000개의 JPA 엔티티 리스트
+        List<Entity> entityList = fetchEntities();
+
+        CompletableFuture<Double> averageA = CompletableFuture.supplyAsync(() -> {
+            List<A> aList = entityList.stream()
+                    .limit(200)
+                    .map(Entity::toA)
+                    .collect(Collectors.toList());
+            return aList.stream().mapToDouble(A::getValue).average().orElse(0);
+        });
+
+        CompletableFuture<Double> averageB = CompletableFuture.supplyAsync(() -> {
+            List<B> bList = entityList.stream()
+                    .skip(200)
+                    .limit(300)
+                    .map(Entity::toB)
+                    .collect(Collectors.toList());
+            return bList.stream().mapToDouble(B::getValue).average().orElse(0);
+        });
+
+        CompletableFuture<List<C>> cFuture = CompletableFuture.supplyAsync(() -> {
+            List<C> cList = entityList.stream()
+                    .skip(500)
+                    .map(Entity::toC)
+                    .collect(Collectors.toList());
+            return cList;
+        });
+
+        CompletableFuture<BigDecimal> finalResult = averageA.thenCombine(averageB, (avgA, avgB) -> {
+            BigDecimal avgAasBigDecimal = BigDecimal.valueOf(avgA);
+            BigDecimal avgBasBigDecimal = BigDecimal.valueOf(avgB);
+            return avgAasBigDecimal.add(avgBasBigDecimal);
+        }).thenCombine(cFuture, (avgAB, cList) -> {
+            BigDecimal avgC = cList.stream()
+                    .map(C::getValue)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            return avgAB.add(avgC);
+        });
+
+        try {
+            BigDecimal result = finalResult.get();
+            System.out.println("Final Result: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 가상의 Entity 클래스와 A, B, C DTO 클래스 정의
+    static class Entity {
+        public A toA() { *//* A로 변환 로직 *//* }
+        public B toB() { *//* B로 변환 로직 *//* }
+        public C toC() { *//* C로 변환 로직 *//* }
+    }
+
+    static class A {
+        public double getValue() { *//* 값을 얻는 로직 *//* }
+    }
+
+    static class B {
+        public double getValue() { *//* 값을 얻는 로직 *//* }
+    }
+
+    static class C {
+        public BigDecimal getValue() { *//* 값을 얻는 로직 *//* }
+    }
+
+    static List<Entity> fetchEntities() {
+        // JPA 또는 데이터베이스에서 엔티티를 가져오는 로직
+        return null;
+    }*/
+}
--- src/main/java/me/kbh/javastudy/basic/ch11/v2/ShopTest.java
+++ src/main/java/me/kbh/javastudy/basic/ch11/v2/ShopTest.java
@@ -27,7 +27,7 @@ public class ShopTest {
         );
 
         long start = System.nanoTime();
-        //System.out.println(findPrices(shopList,"BestShop1")); 4s
+        //System.out.println(findPrices(shopList,"BestShop1")); //4s
         //System.out.println(findPricesByStream(shopList,"BestShop1")); 1s
         System.out.println(findPricesByExecutor(shopList, "BestShop1", executor));
 
@@ -51,8 +51,18 @@ public class ShopTest {
                 .collect(Collectors.toList());
     }
 
-    public static List<String> findPricesByExecutor(List<Shop> shopList, String product, Executor executor) {
-        return shopList.parallelStream()
+    public static List<CompletableFuture<String>> findPricesByExecutor(List<Shop> shopList, String product, Executor executor) {
+        return shopList.stream()
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getProduct() + " price is " + shop.getPrice(product), executor))
+                .collect(Collectors.toList());
+    }
+
+    // 각각 비동기적으로 계산하고 모든 비동기 동작이 끝나길 기다린다.
+    // 단일 파이프라인으로 작성해버리면 모든 가격 정보요청이 동기적이고 순차적으로 이루어진다.
+    // CompletableFuture로 각 상점의 정보를 요청해서 완료과 되어야 join()이 결과를 반환하면서 다음 상점의 정보를 요청할 수 있기 때문이다.
+    // 반면 두개의 스트림으로 연산하면 독립적으로 각각 수행이 가능하다
+    public static List<String> findPricesByExecutor2(List<Shop> shopList, String product, Executor executor) {
+        return shopList.stream()
                 .map(shop -> CompletableFuture.supplyAsync(() -> shop.getProduct() + " price is " + shop.getPrice(product), executor))
                 .collect(Collectors.toList())
                 .stream().map(CompletableFuture::join)
